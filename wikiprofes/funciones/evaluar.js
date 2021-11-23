0bc3c7ad54f550a8c7c6a2107375587756ef857a
TikisVal <94882042+TikisVal@users.noreply.github.com>
2021-11-22 21:43:00 -0600

    Delete wikiprofes directory

--- wikiprofes/funciones/evaluar.js
+++
@@ -1,8 +0,0 @@
-function enviarDatos(){
-    var puntuacion = document.formulario.estrellas.value;
-    var comentario = document.formulario.comentario.value;
-
-    document.formulario.method = 'post';
-	document.formulario.action = '../agregar_comentario.php';
-	document.formulario.submit();
-}
